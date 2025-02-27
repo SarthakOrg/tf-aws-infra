c817656fe01a5ccdbf576c5db7b1697b1612a32d
Sarthak Deshmukh <deshmukh.sar@northeastern.edu>
2025-02-26 20:25:20 -0500

    Removed protection from api termination

--- resources/ec2/main.tf
+++ resources/ec2/main.tf
@@ -1,10 +1,9 @@
 resource "aws_instance" "app" {
-  ami                     = var.ami_id
-  instance_type           = var.instance_type
-  subnet_id               = var.subnet_id
-  key_name                = var.key_name
-  vpc_security_group_ids  = var.security_group_ids
-  disable_api_termination = true
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  subnet_id              = var.subnet_id
+  key_name               = var.key_name
+  vpc_security_group_ids = var.security_group_ids
 
   root_block_device {
     volume_size           = 25
