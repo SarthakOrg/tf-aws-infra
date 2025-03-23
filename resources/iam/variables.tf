b09f2ddd34dbbc7269ffa0cb04f233f8e2cb1544
Sarthak Deshmukh <deshmukh.sar@northeastern.edu>
2025-03-22 22:16:28 -0400

    Added cloudwatch policy in ec2

--- resources/iam/variables.tf
+++ resources/iam/variables.tf
@@ -1,3 +1,11 @@
 variable "ec2_s3_policy_arn" {
   description = "ARN of the custom S3 policy for EC2 user"
 }
+
+variable "iam_policy_arn_CloudWatchLogPolicy" {
+  description = "ARN of the custom CloudWatch Log policy"
+}
+
+variable "iam_policy_arn_CloudWatchMetricsPolicy" {
+  description = "ARN of the custom CloudWatch Metrics policy"
+}
