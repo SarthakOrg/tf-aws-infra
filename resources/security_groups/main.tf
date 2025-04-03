bd2f17d8262de634a08f1982bf4d2a36317719c1
Sarthak Deshmukh <deshmukh.sar@northeastern.edu>
2025-04-02 20:33:13 -0400

    Terraform format fix

--- resources/security_groups/main.tf
+++ resources/security_groups/main.tf
@@ -4,10 +4,10 @@ resource "aws_security_group" "app_sg" {
   vpc_id      = var.vpc_id
 
   ingress {
-    description     = "Allow SSH"
-    from_port       = 22
-    to_port         = 22
-    protocol        = "tcp"
+    description = "Allow SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
     # cidr_blocks     = ["0.0.0.0/0"]
     security_groups = [aws_security_group.lb_sg.id]
   }
